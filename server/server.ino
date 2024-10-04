6254e32abdcfab1aea1c0eb0246e56cddbca8946
Zen <zenn.luu@gmail.com>
2024-10-04 17:17:03 +0300

    check grid for win

--- server/server.ino
+++ server/server.ino
@@ -1,6 +1,41 @@
 // Set up communication speed same as client
 const int baud_rate = 115200;
 
+char grid[9] = {
+  '0', '0', '0',
+  '0', '0', '0',
+  '0', '0', '0'
+};
+
+char win = 'n';
+
+bool checkForWin(){
+  char C = 'o';
+  for (int k = 0; k < 2; k++){
+    // check horizontal lines
+    for(int i = 0; i < 3; i++){
+      if(grid[i*3] == grid[i*3 + 1] == grid[i*3 + 2] == C){
+        win = C;
+        return true;
+      }
+    }
+    // check vertical lines
+    for(int i = 0; i < 3; i++){
+      if(grid[i] == grid[i + 3] == grid[i + 6] == C){
+        win = C;
+        return true;
+      }
+    }
+    // check diagonals
+    if((grid[0] == grid[4] == grid[8] == C) || (grid[2] == grid[4] == grid[6] == C)){
+      win = C;
+      return true;
+    }
+    C = 'x'
+  }
+  return false;
+}
+
 void setup() {
   // Start the UART communication with same speed as client
   Serial.begin(baud_rate);
@@ -11,7 +46,7 @@ void loop() {
   if (Serial.available()) {
     // Read incoming message from client
     String received_message = Serial.readStringUntil('\n');
-    
+ 
     // Modify recieved message and send it back to client
     String modified_message = received_message + " - Hello from ESP32!";
     Serial.println(modified_message);

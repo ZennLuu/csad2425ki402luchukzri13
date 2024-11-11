74a89806b453228a3ceec3af5d8f396d2fb3a3fd
Zen <zenn.luu@gmail.com>
2024-11-11 17:43:10 +0200

    CI

--- ci/buildServer.bat
+++ ci/buildServer.bat
@@ -1,10 +1,25 @@
 @echo off
 REM Exit on error
 setlocal enabledelayedexpansion
-set ArduinoProjectPath=..\server
-set ArduinoBoard=esp32:esp32:esp32
+set ESP32ProjectPath=..\server
+set Board=esp32:esp32:esp32
 set requiredCore=esp32:esp32
 
+:COM_CHECK
+REM Check for available COM port
+for /f "tokens=*" %%i in ('powershell -Command "Get-PnpDevice | Where-Object { $_.FriendlyName -like 'USB-SERIAL CH340*' -and $_.Status -eq 'OK' } | ForEach-Object { $_.FriendlyName }"') do (
+    echo %%i | findstr "COM[0-9]" >nul
+    if !errorlevel! equ 0 (
+        for /f "tokens=2 delims=()" %%j in ("%%i") do set comPortNumber=%%j
+        echo Found device on port: !comPortNumber!
+        goto COM_GOOD
+    )
+)
+
+echo Connected ESP32 not found.
+exit /b
+
+:COM_GOOD
 REM Check if arduino-cli is installed
 where arduino-cli >nul 2>&1
 if %errorlevel% neq 0 (
@@ -12,12 +27,15 @@ if %errorlevel% neq 0 (
     exit /b
 )
 
+echo arduino-cli checked.
+
 REM Check if the required core is installed
 for /f "tokens=*" %%i in ('arduino-cli core list') do (
     set output=%%i
     echo !output! | findstr /c:"%requiredCore%" >nul
     if !errorlevel! equ 0 (
-        goto COM_CHECK
+	echo %requiredCore% core checked.
+        goto BUILD
     )
 )
 
@@ -27,29 +45,16 @@ arduino-cli config set board_manager.additional_urls "https://raw.githubusercont
 arduino-cli core update-index
 arduino-cli core install %requiredCore%
 
-:COM_CHECK
-REM Check for available COM port
-for /f "tokens=*" %%i in ('powershell -Command "Get-PnpDevice | Where-Object { $_.FriendlyName -like 'USB-SERIAL CH340*' -and $_.Status -eq 'OK' } | ForEach-Object { $_.FriendlyName }"') do (
-    echo %%i | findstr "COM[0-9]" >nul
-    if !errorlevel! equ 0 (
-        for /f "tokens=2 delims=()" %%j in ("%%i") do set comPortNumber=%%j
-        echo Found device on port: !comPortNumber!
-        goto BUILD
-    )
-)
-
-echo Connected ESP32 not found.
-exit /b
 
 :BUILD
-REM Build Arduino Project if COM-port available
-echo Building Arduino project...
-cd %ArduinoProjectPath%
-arduino-cli compile --fqbn %ArduinoBoard% .
+REM Build ESP32 Project
+echo Building ESP32 project...
+cd %ESP32ProjectPath%
+arduino-cli compile --fqbn %Board% .
 
 REM Upload to ESP32
 echo Uploading to ESP32 board on port %comPortNumber%...
-arduino-cli upload -p %comPortNumber% --fqbn %ArduinoBoard% .
+arduino-cli upload -p %comPortNumber% --fqbn %Board% .
 
 cd ..\ci
 echo Server build process completed successfully.

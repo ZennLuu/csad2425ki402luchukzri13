a6ea596057079393e771a2a81e75805f099c2684
Zen <zenn.luu@gmail.com>
2024-11-29 16:08:59 +0200

    buildServer change from "GetPnp-device" to "arduino-cli board list"

--- ci/buildServer.bat
+++ ci/buildServer.bat
@@ -6,20 +6,6 @@ set ESP32ProjectPath=..\server
 set Board=esp32:esp32:esp32
 set requiredCore=esp32:esp32
 
-REM Check for available COM port
-for /f "tokens=*" %%i in ('powershell -Command "Get-PnpDevice | Where-Object { $_.FriendlyName -like 'USB-SERIAL CH340*' -and $_.Status -eq 'OK' } | ForEach-Object { $_.FriendlyName }"') do (
-    echo %%i | findstr "COM[0-9]" >nul
-    if !errorlevel! equ 0 (
-        for /f "tokens=2 delims=()" %%j in ("%%i") do set comPortNumber=%%j
-        echo Found device on port: !comPortNumber!
-        goto COM_GOOD
-    )
-)
-
-echo Connected ESP32 not found.
-exit /b
-
-:COM_GOOD
 REM Check if arduino-cli is installed
 where arduino-cli >nul 2>&1
 if %errorlevel% neq 0 (
@@ -29,6 +15,20 @@ if %errorlevel% neq 0 (
 
 echo arduino-cli checked.
 
+REM Check for available COM port
+for /f "tokens=1,2 delims= " %%i in ('arduino-cli board list') do (
+    echo %%i | findstr "COM[0-9]" >nul
+    if !errorlevel! equ 0 (
+        set comPortNumber=%%i
+        echo Found device on port: !comPortNumber!
+        goto COM_GOOD
+    )
+)
+
+echo Connected board not found.
+exit /b
+
+:COM_GOOD
 REM Check if the required core is installed
 for /f "tokens=*" %%i in ('arduino-cli core list') do (
     set output=%%i
@@ -39,25 +39,22 @@ for /f "tokens=*" %%i in ('arduino-cli core list') do (
     )
 )
 
-REM Download core if required
 echo Core %requiredCore% is NOT installed. Installing it now...
 arduino-cli config init
 arduino-cli config set board_manager.additional_urls "https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json"
 arduino-cli core update-index
 arduino-cli core install %requiredCore%
 
-
 :BUILD
-REM Build ESP32 Project
-echo Building ESP32 project...
+echo Building Server...
 cd %ESP32ProjectPath%
 arduino-cli compile --fqbn %Board% .
 
-REM Upload to ESP32
-echo Uploading to ESP32 board on port %comPortNumber%...
+echo Uploading bytecode to the board on port %comPortNumber%...
 arduino-cli upload -p %comPortNumber% --fqbn %Board% .
 
 cd ..\ci
+
 echo Server build process completed successfully.
 endlocal
 
